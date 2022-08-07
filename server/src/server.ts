0771a9c6b451f360af5de172da9d41b2b4970d15
artioki <kmm.19@uni-dubna.ru>
2022-08-08 02:53:09 +0300

    feat: add server with prisma and DockerFile

---
+++ server/src/server.ts
@@ -0,0 +1,104 @@
+import express from "express"
+import { PrismaClient } from "@prisma/client"
+
+const app = express()
+const PORT = 5000
+
+const prisma = new PrismaClient()
+
+app.use(express.json());
+
+app.get('/', (req,  res) => {
+  res.json('hello there')
+})
+
+app.get('/comments/:id.:json', async (req, res ) => {
+  let id = Number(req.params["id"])-1;
+  let json = req.params["json"];
+
+  if(!(id+1) || id < 0 || id > 11 || json !=='json'){
+    res.json('данных нет')
+  }
+  try {
+    const users = await prisma.itemNews.findMany({
+      skip: 30*id,
+      take: 30,
+      where:{type:'comment'},
+      orderBy:{time:"asc"}
+    })
+    res.json(users)
+  } catch(err) {
+    console.log(err)
+  }
+})
+app.get('/newest/:id.:json', async (req, res ) => {
+  let id = Number(req.params["id"])-1;
+  let json = req.params["json"];
+
+  if(!(id+1) || id < 0 || id > 11 || json !=='json'){
+    res.json('данных нет')
+  }
+  try {
+    const newest = await prisma.itemNews.findMany({
+      skip: 30*id,
+      take: 30,
+      where:{type: {in:['link','job','story','poll','pollopt']}},
+      orderBy:{time:"asc"}
+    })
+    res.json(newest)
+  } catch(err) {
+    console.log(err)
+  }
+
+})
+app.get('/item/:id.json',async (req,res)=>{
+  let id = Number(req.params["id"]);
+  try {
+    const users = await prisma.itemNews.findFirst({
+      where:{
+        parent_id:null,
+        id
+      },
+      include:{
+        comments:{
+          where:{
+            level:1
+          },
+          orderBy:{time:"asc"},
+          include:{
+            comments:{
+              where:{
+                level:2
+              },
+              orderBy:{time:"asc"},
+              include:{
+                comments:{
+                  where:{
+                    level:3
+                  },
+                  orderBy:{time:"asc"},
+                  include:{
+                    comments: {
+                      where:{
+                        level:4
+                      },
+                      orderBy:{time:"asc"},
+                    }
+
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    })
+    res.json(users)
+  } catch(err) {
+      console.log(err)
+    }
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
