9b147920a7f4beb1a6c9d1e4e85c6f824118ef97
artioki <kmm.19@uni-dubna.ru>
2022-08-09 01:12:04 +0300

    fix(useTypeDispatch.ts): убрал warning any

--- my-app/src/hooks/useTypedDispatch.ts
+++ my-app/src/hooks/useTypedDispatch.ts
@@ -3,5 +3,5 @@ import { useDispatch } from 'react-redux';
 import { AnyAction } from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
 
-export type TypedDispatch = ThunkDispatch<FeedAction,any, AnyAction>;
+export type TypedDispatch = ThunkDispatch<FeedAction,never, AnyAction>;
 export const useTypedDispatch = () => useDispatch<TypedDispatch>();
